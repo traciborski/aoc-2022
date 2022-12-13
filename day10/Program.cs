156a7a6bacd5635a712091c0e3314eb2cdd73255
Tomasz Raciborski <tomasz.raciborski@softwareone.com>
2022-12-13 01:25:36 +0100

    day 10

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,25 +1,43 @@
 ﻿var lines = await File.ReadAllLinesAsync("input");
-var adds = new int[lines.Count() + 2];
+var adds = new int[lines.Count() * 2];
 var checkpoints = new[] { 20, 60, 100, 140, 180, 220 };
-//var checkpoints = new[] { 7 }; // 
 var result = 0;
 var x = 1; // register
+var cycle = 0;
 for (int i = 0; i < lines.Length; i++)
 {
     var line = lines[i];
     var command = Parse(line);
-    adds[i + command.cycles] += command.value;
+    cycle += command.cycles;
+    adds[cycle] = command.value;
 }
+
+var registers = new int[adds.Length];
 for (int i = 0; i < adds.Length; i++)
 {
+    x += adds[i];
+    registers[i] = x;
     if (checkpoints.Contains(i + 1))
         result += (i + 1) * x;
-    
-    x += adds[i];
 }
-// 32980 too high
-// 10760 too low
+
 Console.WriteLine(result);
+Console.WriteLine();
+
+for (int i = 0; i < adds.Length; i++)
+{
+    var pos_x = i % 40;
+    if (pos_x == 0)
+        Console.WriteLine();
+    var visible = Visible(registers[i], pos_x);
+    Console.Write(visible ? 'X' : '.');
+}
+
+bool Visible(int middle, int pos)
+{
+    return middle - 1 <= pos && pos <= middle + 1;
+}
+
 (int value, int cycles) Parse(string line)
 {
     if (line == "noop") return (0, 1);
