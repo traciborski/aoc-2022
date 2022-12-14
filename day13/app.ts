5d34f7a9fe6ab0b13067e3bee1a39d5ce6fb35f8
Tomasz Raciborski <tomasz.raciborski@softwareone.com>
2022-12-14 01:38:23 +0100

    day 13 p 1

--- day13/app.ts
+++ day13/app.ts
@@ -1,17 +1,38 @@
 import { readFileSync } from 'fs';
 
 const lines = readFileSync('./input', 'utf-8').split("\n");
-
-for (let i = 1; i < lines.length; i = i + 3) {
-    let line = lines[i]
-    // console.log(line);
-    let a1 = eval(line);
+let sum = 0;
+let index = 1;
+for (let i = 0; i < lines.length; i = i + 3) {
+    let a1 = eval(lines[i]);
     let a2 = eval(lines[i + 1]);
-
-    for (var j = 0; j < a1.length; j++) {
-        console.log(a1[j]+' '+a2[j]);
+    if (inOrder(a1, a2)) {
+        sum += index;
     }
-
-    // console.log(a1[0]);
+    index++;
 }
+console.log(sum);
 
+function inOrder(a1: any, a2: any): boolean {
+    for (var i = 0; i < a1.length; i++) {
+        var x = a1[i];
+        var y = a2[i];
+        if (Number.isFinite(x) && Number.isFinite(y)) {
+            if (x < y) return true;
+            if (x > y) return false;
+        } else if (Array.isArray(x) && Array.isArray(y)) {
+            if (!inOrder(x, y)) return false;
+        } else if (Array.isArray(x) && Number.isFinite(y)) {
+            if (!inOrder(x, [y])) return false;
+        } else if (Array.isArray(y) && Number.isFinite(x)) {
+            if (!inOrder([x], y)) return false;
+        } else if (x === undefined) { 
+            return true;
+        } else if (y === undefined) {
+            return true;
+        } else {
+            return true;
+        }
+    }
+    return true;
+}
