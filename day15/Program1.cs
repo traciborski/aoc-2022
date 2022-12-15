e8dfedd9e738e9516d74f4d7b91b6b1c6b024eb6
Tomasz Raciborski <tomasz.raciborski@softwareone.com>
2022-12-16 00:47:51 +0100

    day 15 part 2

---
+++ day15/Program1.cs
@@ -0,0 +1,38 @@
+﻿//using System.Text.RegularExpressions;
+
+//var Y = 2000000;
+
+//var lines = File.ReadAllLines("input");
+//var map = new Dictionary<int, char>();
+
+//foreach (var line in lines)
+//{
+//    var match = Regex.Match(line, @"Sensor at x=(\d+), y=(\d+): closest beacon is at x=(-*\d+), y=(-*\d+)");
+//    var sx = Parse(match, 1);
+//    var sy = Parse(match, 2);
+//    var bx = Parse(match, 3);
+//    var by = Parse(match, 4);
+//    if (by == Y) Map(bx, 'B');
+//    var d = Math.Abs(bx - sx) + Math.Abs(by - sy);
+//    var (x_min, x_max) = MinMax(sx, sy, d);
+//    for (int x = x_min; x <= x_max; x++)
+//        Map(x, 'X');
+//}
+
+//Console.WriteLine(map.Count(x => x.Value == 'X'));
+//int Parse(Match match, int i) => int.Parse(match.Groups[i].Value);
+
+//(int min, int max) MinMax(int sx, int sy, int d) // min max x on Y to mark X
+//{
+//    var dy = Math.Abs(Y - sy);
+//    var x1 = sx - d + dy;
+//    var x2 = sx + d - dy;
+//    return (Math.Min(x1, x2), Math.Max(x1, x2));
+//}
+//void Map(int x, char c)
+//{
+//    if (!map.ContainsKey(x))
+//        map[x] = c;
+//    else if (c != 'X') // beacon and sensor can overide, X can't
+//        map[x] = c;
+//}
---
+++ day15/Program2.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input");
+var size = 4_000_000;
+var map = new Dictionary<int, List<(int from, int to)>>();
+for (var i = 0; i <= size; i++) map.Add(i, new List<(int x, int y)>());
+
+foreach (var line in lines)
+{
+    var match = Regex.Match(line, @"Sensor at x=(\d+), y=(\d+): closest beacon is at x=(-*\d+), y=(-*\d+)");
+    var sx = Parse(match, 1);
+    var sy = Parse(match, 2);
+    var bx = Parse(match, 3);
+    var by = Parse(match, 4);
+
+    var d = Math.Abs(bx - sx) + Math.Abs(by - sy);
+
+    for (int y = Math.Max(0, sy - d); y <= Math.Min(sy + d, size); y++)
+    {
+        var (x_min, x_max) = MinMax(sx, sy, d, y);
+        map[y].Add((x_min, x_max));
+    }
+}
+for (int y = 0; y <= map.Count; y++)
+{
+    map[y] = map[y].OrderBy(x => x.from).ToList();
+    var r = 0;
+    for (int x = 0; x < map[y].Count; x++)
+    {
+        if (map[y][x].from > r)
+        {
+            Console.WriteLine(4_000_000 * (map[y][x].from - 1) + y);
+            return;
+        }
+        else
+        {
+            r = Math.Max(r, map[y][x].to);
+        }
+    }
+}
+
+int Parse(Match match, int i) => int.Parse(match.Groups[i].Value);
+
+(int min, int max) MinMax(int sx, int sy, int d, int y) // min max x on Y to mark X
+{
+    var dy = Math.Abs(y - sy);
+    var x1 = sx - d + dy;
+    var x2 = sx + d - dy;
+    return (Math.Min(x1, x2), Math.Max(x1, x2));
+}
