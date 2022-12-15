19587afa8eb247380dca356e2974ca0e91810124
Tomasz Raciborski <tomasz.raciborski@softwareone.com>
2022-12-15 23:08:45 +0100

    day 15 p1

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,66 +1,38 @@
 ﻿using System.Text.RegularExpressions;
 
+var Y = 2000000;
+
 var lines = File.ReadAllLines("input");
-var map = new List<(int x, char sign)>();
-var row = 2000000;
-var beacon_found = false;
-int beacon_x, beacon_y;
+var map = new Dictionary<int, char>();
 
 foreach (var line in lines)
 {
-    Console.WriteLine(line);
     var match = Regex.Match(line, @"Sensor at x=(\d+), y=(\d+): closest beacon is at x=(-*\d+), y=(-*\d+)");
     var sx = Parse(match, 1);
     var sy = Parse(match, 2);
     var bx = Parse(match, 3);
     var by = Parse(match, 4);
-    MarkOnRow(sx, sy, bx, by);
+    if (by == Y) Map(bx, 'B');
+    var d = Math.Abs(bx - sx) + Math.Abs(by - sy);
+    var (x_min, x_max) = MinMax(sx, sy, d);
+    for (int x = x_min; x <= x_max; x++)
+        Map(x, 'X');
 }
 
-int Parse(Match match, int i)
-{
-    return int.Parse(match.Groups[i].Value);
-}
+Console.WriteLine(map.Count(x => x.Value == 'X'));
+int Parse(Match match, int i) => int.Parse(match.Groups[i].Value);
 
-Console.WriteLine(map.Count(x => x.sign == 'X'));
-
-void MarkOnRow(int sx, int sy, int bx, int by)
+(int min, int max) MinMax(int sx, int sy, int d) // min max x on Y to mark X
 {
-    beacon_x = bx;
-    beacon_y = by;
-    var radius = 1;
-    beacon_found = false;
-    while (true)
-    {
-        for (int i = 0; i <= radius; i++)
-        {
-            Mark(sx - radius, sy + i);
-            Mark(sx - radius, sy - i);
-            Mark(sx + radius, sy + i);
-            Mark(sx + radius, sy - i);
-            Mark(sx - i, sy + radius);
-            Mark(sx - i, sy - radius);
-            Mark(sx + i, sy + radius);
-            Mark(sx + i, sy - radius);
-        }
-        if (beacon_found) return;
-        radius++;
-    }
+    var dy = Math.Abs(Y - sy);
+    var x1 = sx - d + dy;
+    var x2 = sx + d - dy;
+    return (Math.Min(x1, x2), Math.Max(x1, x2));
 }
-void Mark(int x, int y)
+void Map(int x, char c)
 {
-    if (x == beacon_x && y == beacon_y)
-    {
-        beacon_found = true;
-        return;
-    }
-    if (y == row)
-    {
-        if (!map.Any(a => a.x == x))
-            map.Add((x, 'X'));
-    }
-}
-int Increment(int x1, int x2)
-{
-    return x2.CompareTo(x1);
+    if (!map.ContainsKey(x))
+        map[x] = c;
+    else if (c != 'X') // beacon and sensor can overide, X can't
+        map[x] = c;
 }
